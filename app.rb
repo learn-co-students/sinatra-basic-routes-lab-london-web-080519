0f3eda7f38f608e212a569787fc33519f78461da
felipeldn <felipeldn91@gmail.com>
2019-08-27 16:00:23 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Felipe."
+    end
+
+    get '/hometown' do
+        "My hometown is Salvador, Brazil."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is 'Some Feeling' by Mild Orange"
+    end
+
 end
